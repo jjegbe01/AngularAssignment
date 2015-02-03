8ffe5a0e7a4e0ff58c13821dd0bb2a8137d65518
jjegbe01 <jjegbe01@louisville.edu>
2015-02-03 11:34:59 -0500

    Completed challenge 1.4
    Page still won't display correctly for some reason? Code is identical to
    completed example, but it only prints the {{angular code}} instead of
    the object values.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
-var app = angular.module('store',[]);
-var app = angular.module('gemStore',[]);
+(function()
+{
+
+//Modules
+var app = angular.module('store',[]); //module for example 0
+var app = angular.module('gemStore',[]); //module for gem store
+
+//Objects
+var gem = 
+{
+	name: 'Dodecahedron',
+	price: 2.95,
+	description: '. . .',
+}
+
+//Controllers
+app.controller('StoreController', function()
+{
+	this.product = gem;
+});
+
+
+});
+
