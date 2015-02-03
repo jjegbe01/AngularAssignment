b9a593182aeb43d519dd354eadf6dd83ef5ef95b
jjegbe01 <jjegbe01@louisville.edu>
2015-02-03 11:45:20 -0500

    Fixed a syntax error.
    It was an error with my function wrapper inside my app.js file that was
    causing angular to not load on any page. I wanted to fix this before
    moving on in the tutorial.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,18 @@
-(function()
-{
+(function(){
 
 //Modules
 var app = angular.module('store',[]); //module for example 0
 var app = angular.module('gemStore',[]); //module for gem store
 
 //Objects
-var gem = 
-{
-	name: 'Dodecahedron',
-	price: 2.95,
-	description: '. . .',
-}
+ var gem = { name: 'Azurite', price: 2.95 };
 
 //Controllers
 app.controller('StoreController', function()
-{
-	this.product = gem;
-});
+                 {
+                   this.product = gem;
+                 });
 
 
-});
 
+})();
