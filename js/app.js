983cc5f51e053bb50ab64f926db973eea8e5e4a3
jjegbe01 <jjegbe01@louisville.edu>
2015-02-03 12:00:39 -0500

    Completed challenge 1.6
    Added hide function for items that are out of stock, and a hide function
    for the add to cart button for items that are not for sale.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var app = angular.module('store',[]); //module for example 0
 var app = angular.module('gemStore',[]); //module for gem store
 
 //Objects
- var gem = { name: 'Azurite', price: 2.95 };
+ var gem = { name: 'Azurite', price: 110.50, canPurchase: false, soldOut: true };
 
 //Controllers
 app.controller('StoreController', function()
